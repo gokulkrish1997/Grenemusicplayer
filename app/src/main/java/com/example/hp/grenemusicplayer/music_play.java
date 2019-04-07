702b5bf56998f2869ddbbe78fc3864a06b508f65
Gokul <neyveli.gokulakrishnan@gmail.com>
2019-04-07 16:20:58 +0530

    first commit

---
+++ app/src/main/java/com/example/hp/grenemusicplayer/music_play.java
@@ -0,0 +1,14 @@
+package com.example.hp.grenemusicplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class music_play extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.music_player);
+    }
+}
